001f0ee13c6cd7d27c05d158b3d55bb09e381b22
youthlion <2877402302@qq.com>
2018-05-07 12:57:50 +0800

    www

--- www/static/orm.py
+++ www/static/orm.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__auther__='Youth Lion'
+
 import asyncio,logging
 
 import aiomysql
@@ -84,8 +89,8 @@ class IntegerField(Field):
 
 class FloatField(Field):
 
-    def __init__(self,name=None,default=None):
-        super().__init__(name,'text',False,default)
+    def __init__(self,name=None,default=0.0):
+        super().__init__(name,'text',primary_key,default)
 
 class TextField(Field):
 
@@ -100,15 +105,15 @@ class ModelMetaclass(type):
         tableName=attrs.get('__table__',None) or name
         logging.info('found model:%s(table:%s)'%(name,tableName))
         mappings=dict()
-        field=[]
+        fields=[]
         primaryKey=None
         for k,v in attrs.items():
             if isinstance(v,Field):
-                logging.info(' found mapping:%s ==>%s'%(k,v))
+                logging.info(' found mapping:%s ==> %s'%(k,v))
                 mapping[k]=v
                 if v.primary_key:
                     if primaryKey:
-                        raise StandardError('Duplicate primary key for field:%s'%k)
+                        raise StandardError('Duplicate primary key for field:%s' % k)
                     primaryKey=k
                 else:
                     fields.append(k)
@@ -144,27 +149,27 @@ class Model(dict,metaclass=ModelMetaclass):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError(r"'Model' object has no attribute '%s'"% key)
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
     def  __setattr__(self,key,value):
         self[key]=value
 
-    def getValue(self,key,value):
-        self[key]=value
+    def getValue(self,key):
+        return getattr(self,key,None)
 
     def getValueOrDefault(self,key):
         value=getattr(self,key,None)
         if value is None:
             field=self.__mappings__[key]
             if field.default is not None:
-                value=field.default()if callable (field.default)else field.default
+                value=field.default() if callable(field.default) else field.default
                 logging.debug('using default value for %s:%s'%(key,str(value)))
                 setattr(self,key,value)
         return value
 
     @classmethod
     async def findAll(cls,where=None,args=None,**kw):
-        ' find object by where clause. '
+        ' find objects by where clause. '
         sql=[cls.__select__]
         if where:
             sql.append('where')
@@ -186,13 +191,13 @@ class Model(dict,metaclass=ModelMetaclass):
                 args.extend(limit)
             else:
                 raise ValueError('Invalid limit value:%s'%str(limit))
-
         rs=await select(' ',join(sql),args)
         return [cls(**r)for r in rs]
+    
     @classmethod
     async def findNumber(cls,selectField,where=None,args=None):
         ' find number by select and where. '
-        sql=['select %s _num_ from  `%s`' % (selectField,cls.__table)]
+        sql=['select %s _num_ from  `%s`' % (selectField,cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
@@ -201,13 +206,7 @@ class Model(dict,metaclass=ModelMetaclass):
             return None
         return rs[0]['_num_']
 
-    @classmethod
-    async def find(cls,pk):
-        ' find object by primary key. '
-        rs=await select('%s where `%s`=?'%(cls.__select__,cls.__primaty_key__),[pk],1)
-        if len(rs)==0:
-            return None
-        return rs[0]['_num_']
+
     @classmethod
     async def find(cls,pk):
         ' find object by primary key. '
@@ -215,22 +214,23 @@ class Model(dict,metaclass=ModelMetaclass):
         if len(rs)==0:
             return None
         return cls(**rs[0])
+
     async def save(self):
-        args=list(map(self.getvalueOrDefault,self.__field__))
-        args=append(self.getValueOrDefault(self.__primary_key__))
+        args=list(map(self.getvalueOrDefault,self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
         rows=await execute(self.__insert__,args)
         if rows !=1:
-            logging.warn('field to insert record:affected rows:%s'%rows)
+            logging.warn('field to insert record:affected rows:%s' % rows)
 
     async def update(self):
-        args=list(map(self.getValue,self.__field__))
+        args=list(map(self.getValue,self.__fields__))
         args.append(self.getValue(self.__primary_key__))
         rows=await execute(self.__update__,args)
         if rows!=1:
-                        logging.warn('failed to update by primary key:affect rows:%s'%rows)
+            logging.warn('failed to update by primary key:affect rows:%s'%rows)
 
     async def romove(self):
-                        args=[self.getValue(self.__primary_key__)]
-                        rows=await execute(self.__delect__,args)
-                        if row!=1:
-                            logging.warn('failed to remove by primary key:affected rows:%s'% rows)
+        args=[self.getValue(self.__primary_key__)]
+        rows=await execute(self.__delete__,args)
+        if row!=1:
+            logging.warn('failed to remove by primary key:affected rows:%s'% rows)
