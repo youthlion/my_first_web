a2ea8a3fb93b480f2a4425680bb6995dc709557f
youthlion <2877402302@qq.com>
2018-05-08 23:35:05 +0800

    重做

--- www/static/models.py
+++ www/static/models.py
@@ -1,50 +1,97 @@
 #!/usr/bin/env python3
-#-*-coding:utf-8-*-
+
+# -*- coding: utf-8 -*-
+
+
 
 '''
-Model for usr,blog comment.
+
+Models for user, blog, comment.
+
 '''
-__author__='Youth Lion'
 
 
-import time,uuid
 
-from orm import Model,StringField,BooleanField,FloatField,TextField
+__author__ = 'Michael Liao'
+
+
+
+import time, uuid
+
+
+
+from orm import Model, StringField, BooleanField, FloatField, TextField
+
 
 
 def next_id():
-    return '%015d%s000' % (int(time.time()*1000),uuid.uuid4().hex)
+
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
+
+
 
 class User(Model):
-    __table__='users'
-    
-    id=StringField(primary_key=True,default=next_id,ddl='varchar(50)')
-    email=StringField(ddl='varchar(50)')
-    passwd=StringField(ddl='varchar(50)')
-    admin=BooleanField()
-    name=StringField(ddl='varchar(50)')
-    image=StringField(ddl='varchar(500)')
-    created_at=FloatField(default=time.time)
+
+    __table__ = 'users'
+
+
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+
+    email = StringField(ddl='varchar(50)')
+
+    passwd = StringField(ddl='varchar(50)')
+
+    admin = BooleanField()
+
+    name = StringField(ddl='varchar(50)')
+
+    image = StringField(ddl='varchar(500)')
+
+    created_at = FloatField(default=time.time)
+
+
 
 class Blog(Model):
-    __table__='blogs'
 
-    id=StringField(primary_key=True,default=next_id,ddl='varchar(50)')
-    user_id=StringField(ddl='varchar(50)')
-    user_name=StringField(ddl='varchar(50)')
-    user_image=StringField(ddl='varchar(500)')
-    name=StringField(ddl='varchar(50)')
-    summary=StringField(ddl='varchar(200)')
-    content=TextField()
-    created_at=FloatField(default=time.time)
+    __table__ = 'blogs'
+
+
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+
+    user_id = StringField(ddl='varchar(50)')
+
+    user_name = StringField(ddl='varchar(50)')
+
+    user_image = StringField(ddl='varchar(500)')
+
+    name = StringField(ddl='varchar(50)')
+
+    summary = StringField(ddl='varchar(200)')
+
+    content = TextField()
+
+    created_at = FloatField(default=time.time)
+
+
 
 class Comment(Model):
-    __table__='comment'
 
-    id=StringField(primary_key=True,default=next_id,ddl='varchar(50)')
-    blog_id=StringField(ddl='varchar(50)')
-    user_id=StringField(ddl='varchar(50)')
-    user_name=StringField(ddl='varchar(50)')
-    user_image=StringField(ddl='varchar(500)')
-    connect=TextField()
-    created_at=FloatField(default=time.time)
+    __table__ = 'comments'
+
+
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+
+    blog_id = StringField(ddl='varchar(50)')
+
+    user_id = StringField(ddl='varchar(50)')
+
+    user_name = StringField(ddl='varchar(50)')
+
+    user_image = StringField(ddl='varchar(500)')
+
+    content = TextField()
+
+    created_at = FloatField(default=time.time)
